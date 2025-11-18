afa90025f81de0270a3f2a6c0c11c2e4f0cd642b
AshokBabu M <ashokb.aix@gmail.com>
2025-11-18 17:30:02 -0500

    modules supporting files has been added
    Moudles supporting files helps in access the varialble values from the .tf files

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws_instance" "example" {
+   ami = var.ami_value
+    instance_type = var.instance_type_vale
+    subnet_id     = var.subnet_id_value
+
+    tags = {
+      Name = "AWS_EC2_Instance-Module"
+    }
+  
+}
