93c040e2a88b0c0d3b8ed15446b248c31f0d0428
AshokBabu M <ashokb.aix@gmail.com>
2025-11-18 17:23:32 -0500

    Add variables for EC2 instance configuration
    These variable helps to fetch the dynamic values during code commit

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "ami_value" {
+    description = "The AMI ID for the EC2 instance"
+  
+  }
+variable "instance_type_vale" {
+    description = "The instance type for the EC2 instance"
+   
+  }
+variable "subnet_id_value" {
+    description = "The subnet ID for the EC2 instance"
+
+  }
